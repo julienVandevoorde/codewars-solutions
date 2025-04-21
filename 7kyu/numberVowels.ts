67740a4c929f410f94177b56a28e62235e968043
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2025-04-21 16:59:38 +0200

    easy ones

---
+++ 7kyu/numberVowels.ts
@@ -0,0 +1,19 @@
+/*
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+export class Kata {
+    static getCount(str: string): number {
+      let count = 0;
+      for(let letter of str){
+        if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+          count += 1;
+        }
+      }
+      return count;
+    }
+  }
